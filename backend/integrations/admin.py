bb0b0e1600c11e0b3a8b19c8978051965abf0b13
Brandon Pieczka <134565367+Bardemic@users.noreply.github.com>
2025-12-27 01:50:35 -0600

    remove python backend/rename ts to 'backend'

--- backend/integrations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import IntegrationConnection, IntegrationProvider, Tool
-
-admin.site.register(IntegrationProvider)
-admin.site.register(IntegrationConnection)
-admin.site.register(Tool)
